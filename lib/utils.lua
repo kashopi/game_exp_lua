442919523943156d6e039b7bfd11df0376488576
Nacho Rodriguez <kashopi@gmail.com>
2025-04-10 23:02:03 +0200

    Add some random and hashing stuff

--- lib/utils.lua
+++ lib/utils.lua
@@ -1,3 +1,7 @@
+function InitLibs()
+    math.randomseed(os.time())
+end
+
 function Hash(obj)
     -- Returns a hash for the object
     
@@ -11,6 +15,16 @@ function Get_random_number(vmin, vmax)
     return math.random(vmin,vmax)
 end
 
-function InitLibs()
-    math.randomseed(os.time())
+function Get_random_hash()
+    local h = 5381
+    return Hash(tostring(Get_random_number(0, 9999999999)))
+end
+
+function Hash(str)
+    local h = 5381
+
+    for i = 1, #str do
+       h = h*32 + h + str:byte(i)
+    end
+    return h
 end
